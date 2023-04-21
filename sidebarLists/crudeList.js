631092101b1e4b9362eb91ec597f4973a644a07b
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-21 17:24:14 +0530

    adding sidebar lists for crude

---
+++ sidebarLists/crudeList.js
@@ -0,0 +1,39 @@
+const crudeList = [
+  {
+    id: 1,
+    section: "Naptha",
+    slug: "Naphtha",
+  },
+  {
+    id: 2,
+    section: "Propylene",
+    slug: "Propylene",
+  },
+  {
+    id: 3,
+    section: "Ethylene",
+    slug: "ethylene",
+  },
+  {
+    id: 4,
+    section: "VCM & EDC",
+    slug: "vcm&edc",
+  },
+  {
+    id: 5,
+    section: "Steryne",
+    slug: "steryne",
+  },
+  {
+    id: 6,
+    section: "PTA",
+    slug: "pta",
+  },
+  {
+    id: 7,
+    section: "Flow Chart",
+    slug: "flow-chart",
+  },
+];
+
+export default crudeList;
