ad39c730faf13708ac51e5485ce31e94b5dd6fb7
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-22 23:44:47 +0530

    adding loading screen

---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import { ClipLoader } from "react-spinners";
+
+export default function Loading({ children }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Box
+          sx={{
+            display: "grid",
+            placeItems: "center",
+            height: "100vh",
+          }}
+        >
+          <ClipLoader color="#C31815" />
+        </Box>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
+}
