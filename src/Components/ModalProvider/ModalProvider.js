39942e21723f9fb18faffdf8a8290d5107acb0fe
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-22 14:37:34 +0530

    updating useContext

--- src/Components/ModalProvider/ModalProvider.js
+++ src/Components/ModalProvider/ModalProvider.js
@@ -3,15 +3,11 @@ import { createContext, useState } from "react";
 export const ModalContext = createContext();
 
 export function ModalProvider({ children }) {
-  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
   const [isUserProfileModalOpen, setIsUserProfileModalOpen] = useState(false);
 
   return (
     <ModalContext.Provider
       value={{
-        isAuthModalOpen,
-        setIsAuthModalOpen,
-
         isUserProfileModalOpen,
         setIsUserProfileModalOpen,
       }}
