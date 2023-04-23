dde441ff484aaed5d405fb7e059a6eebf92123b5
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-23 20:27:09 +0530

    separate pane lists

---
+++ menuLists/paneList.js
@@ -0,0 +1,52 @@
+const list = [
+  {
+    id: 1,
+    icon: "/Pane/home.svg",
+    txt: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    icon: "/Pane/news.svg",
+    txt: "News",
+    link: "/news",
+  },
+  {
+    id: 3,
+    icon: "/Pane/crude.svg",
+    txt: "Crude & Feedstock",
+    link: "/crude",
+  },
+  {
+    id: 4,
+    icon: "/Pane/global.svg",
+    txt: "Global Bazaar",
+    link: "/global-bazaar",
+  },
+  {
+    id: 5,
+    icon: "/Pane/indian.svg",
+    txt: "Indian Bazaar",
+    link: "/indian-bazaar",
+  },
+  {
+    id: 6,
+    icon: "/Pane/graph.svg",
+    txt: "Graph",
+    link: "/graph",
+  },
+  {
+    id: 7,
+    icon: "/Pane/future.svg",
+    txt: "Future Trend",
+    link: "/future-trend",
+  },
+  {
+    id: 8,
+    icon: "/Pane/services.svg",
+    txt: "Services",
+    link: "/services",
+  },
+];
+
+export default list;
