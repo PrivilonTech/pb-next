24f0b0a65b92ab4641e859a1de7ade4a500992b3
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-23 20:26:50 +0530

    separete footer list

---
+++ menuLists/footerList.js
@@ -0,0 +1,91 @@
+const footerList = [
+  {
+    id: 1,
+    txt: "Company",
+    list: [
+      {
+        id: 1,
+        name: "Blog",
+      },
+      {
+        id: 2,
+        name: "About Us",
+      },
+      {
+        id: 3,
+        name: "Job Openings",
+      },
+      {
+        id: 4,
+        name: "Privacy Policy",
+      },
+    ],
+  },
+  {
+    id: 2,
+    txt: "Community",
+    list: [
+      {
+        id: 1,
+        name: "Another ",
+      },
+      {
+        id: 2,
+        name: "About Us",
+      },
+      {
+        id: 3,
+        name: "Job Openings",
+      },
+      {
+        id: 4,
+        name: "Privacy Policy",
+      },
+    ],
+  },
+  {
+    id: 3,
+    txt: "Categories",
+    list: [
+      {
+        id: 1,
+        name: "Blog",
+      },
+      {
+        id: 2,
+        name: "About Us",
+      },
+      {
+        id: 3,
+        name: "Job Openings",
+      },
+      {
+        id: 4,
+        name: "Privacy Policy",
+      },
+    ],
+  },
+  {
+    id: 4,
+    txt: "Contact us",
+    list: [
+      {
+        id: 1,
+        name: "Blog",
+      },
+      {
+        id: 2,
+        name: "About Us",
+      },
+      {
+        id: 3,
+        name: "Job Openings",
+      },
+      {
+        id: 4,
+        name: "Privacy Policy",
+      },
+    ],
+  },
+];
+export default footerList;
