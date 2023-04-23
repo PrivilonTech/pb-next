72b1c85f5426627e10bb76e60ac24b992a93a618
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-23 14:24:51 +0530

    dummy data for indian bazaar

---
+++ menuLists/indianBazaarList.js
@@ -0,0 +1,39 @@
+const indianBazaarList = [
+  {
+    id: 1,
+    section: "Rate Prediction",
+    slug: "ratePrediction",
+  },
+  {
+    id: 2,
+    section: "City Wise-Metro",
+    slug: "cityWiseMetro",
+  },
+  {
+    id: 3,
+    section: "Historical Data - PP",
+    slug: "historicalDataPP",
+  },
+  {
+    id: 4,
+    section: "Historical Data - LLD",
+    slug: "historicalDataLLD",
+  },
+  {
+    id: 5,
+    section: "Historical Data - HD",
+    slug: "historicalDataHD",
+  },
+  {
+    id: 6,
+    section: "Historical Data PVC",
+    slug: "historicalDataPVC",
+  },
+  {
+    id: 7,
+    section: "Import Calculation",
+    slug: "importCalculation",
+  },
+];
+
+export default indianBazaarList;
