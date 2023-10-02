ea11d997c66e2c9baaec75f8d8f777eb4b59d56b
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-02 23:11:05 +0530

    updating site_logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -77,7 +77,7 @@ function Header() {
               onClick={() => router.push("/")}
               sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
             >
-              <img src={"/Header/logo.svg"} alt="Logo" height={45}></img>
+              <img src={"/Header/logo_new.svg"} alt="Logo" height={100}></img>
             </Box>
           </Box>
           <Box
@@ -191,7 +191,7 @@ function Header() {
               <img src={"/Header/hamburg.svg"} alt="hamburg-icon" />
             </Box>
             <Box onClick={() => router.push("/")} sx={{ margin: "auto" }}>
-              <img src={"/Header/logo.svg"} alt="Logo" height={25}></img>
+              <img src={"/Header/logo_new.svg"} alt="Logo" height={75}></img>
             </Box>
           </Box>
           <HamburgMenu
