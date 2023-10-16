01b5f78d3c4e4441cca427cf382891cdb9723471
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-16 16:20:24 +0530

    adding more items in /future-trend sidebar

--- src/menuLists/futureTrendList.js
+++ src/menuLists/futureTrendList.js
@@ -21,13 +21,43 @@ const futureTrendList = [
   },
   {
     id: 5,
+    section: "PS",
+    slug: "PS",
+  },
+  {
+    id: 6,
     section: "ABS",
     slug: "ABS",
   },
   {
-    id: 6,
+    id: 7,
     section: "EVA",
     slug: "EVA",
+  },
+  {
+    id: 8,
+    section: "PC",
+    slug: "PC",
+  },
+  {
+    id: 9,
+    section: "SAN",
+    slug: "SAN",
+  },
+  {
+    id: 10,
+    section: "Nylon 6 & Nylon 66",
+    slug: "nylon-6-nylon-66",
+  },
+  {
+    id: 11,
+    section: "PBT",
+    slug: "PBT",
+  },
+  {
+    id: 12,
+    section: "POM",
+    slug: "POM",
   },
 ];
 
