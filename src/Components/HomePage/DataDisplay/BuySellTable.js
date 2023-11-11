818377dee84a1f61a06f08539de99e88db4f4131
sukrit101 <sukritsaha27@gmail.com>
2023-11-12 00:02:47 +0530

    fixing items in /buy-sell

--- src/Components/HomePage/DataDisplay/BuySellTable.js
+++ src/Components/HomePage/DataDisplay/BuySellTable.js
@@ -7,7 +7,7 @@ export default function BuySellTable() {
       serialNo: 1,
       product: "PP",
       rmName: "Mrs. Neela Shah",
-      phone: "+91 9313924365",
+      phone: "+91 93139 24 365",
       email: "PP@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/Lagxx3AaZ551prRW0eX64z",
     },
@@ -15,7 +15,7 @@ export default function BuySellTable() {
       serialNo: 2,
       product: "PVC",
       rmName: "S.S.Shaikh",
-      phone: "+91 9510824365",
+      phone: "+91 95108 24 365",
       email: "PVC@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/JqbOQ6o4jTb46G34WG3rEu",
     },
@@ -23,7 +23,7 @@ export default function BuySellTable() {
       serialNo: 3,
       product: "HDPE",
       rmName: "M.S.Shaikh",
-      phone: "+91 9313824365",
+      phone: "+91 93138 24 365",
       email: "HDPE@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/DiD2sDbzsqz90OK0v9zcDN",
     },
@@ -31,7 +31,7 @@ export default function BuySellTable() {
       serialNo: 4,
       product: "LLDPE",
       rmName: "M.S.Shaikh",
-      phone: "+91 9313824365",
+      phone: "+91 93138 24 365",
       email: "LLDPE@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/IY4m3AB81114YHx6lHPjNW",
     },
@@ -39,7 +39,7 @@ export default function BuySellTable() {
       serialNo: 5,
       product: "LDPE",
       rmName: "M.S.Shaikh",
-      phone: "+91 9313824365",
+      phone: "+91 93138 24 365",
       email: "LDPE@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/CteGeeE5PjyKs0uxsrGIyY",
     },
@@ -47,7 +47,7 @@ export default function BuySellTable() {
       serialNo: 6,
       product: "PET",
       rmName: "Mr.Karan Desai",
-      phone: "+91 7984024365",
+      phone: "+91 79840 24 365",
       email: "PET@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/KpSPIbebm1u5GoKdg82780",
     },
@@ -55,7 +55,7 @@ export default function BuySellTable() {
       serialNo: 7,
       product: "PS",
       rmName: "Diksharth Vora",
-      phone: "+91 6352324365",
+      phone: "+91 63523 24 365",
       email: "PS@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/B9ifbkHyoqB87u5185QTXl",
     },
@@ -63,7 +63,7 @@ export default function BuySellTable() {
       serialNo: 8,
       product: "ABS",
       rmName: "Diksharth Vora",
-      phone: "+91 6352324365",
+      phone: "+91 63523 24 365",
       email: "ABS@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/EYGmkTec8eg6fKZLWRCAsm",
     },
@@ -71,7 +71,7 @@ export default function BuySellTable() {
       serialNo: 9,
       product: "ENGG",
       rmName: "Mrs.Pore",
-      phone: "+91 9510424365",
+      phone: "+91 95104 24 365",
       email: "ENGG@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/Hl7PZAkavwV25k9XZMDc1O",
     },
@@ -79,7 +79,7 @@ export default function BuySellTable() {
       serialNo: 10,
       product: "EVA",
       rmName: "Mrs.Pore",
-      phone: "+91 9510424365",
+      phone: "+91 95104 24 365",
       email: "EVA@Polymerbazaar.com",
       whatsapp: "https://chat.whatsapp.com/LHCasT950e2Ck3lQ3MPuYR",
     },
@@ -110,12 +110,16 @@ export default function BuySellTable() {
         <tbody>
           {buySellData.map((item, index) => (
             <tr key={index}>
-              <td style={{ fontSize: ".85rem" }}>{item.serialNo}</td>
+              <td style={{ fontSize: ".85rem", textAlign: "center" }}>
+                {item.serialNo}
+              </td>
               <td style={{ fontSize: ".85rem" }}>{item.product}</td>
               <td style={{ fontSize: ".85rem" }}>{item.phone}</td>
               <td style={{ fontSize: ".85rem" }}>{item.email}</td>
               <td style={{ fontSize: ".85rem" }}>
-                <Link href={item.whatsapp}>{item.whatsapp}</Link>
+                <Link href={item.whatsapp}>
+                  Join Whatsapp Community for {item.product} Buy & Sell
+                </Link>
               </td>
             </tr>
           ))}
