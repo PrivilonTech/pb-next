32fc4e3d06512d63776bf1a7608e1ca5b9f4040a
sukrit101 <sukritsaha27@gmail.com>
2023-11-28 13:48:48 +0530

    updating empty data component

--- src/Components/PaneContent/EmptyData.js
+++ src/Components/PaneContent/EmptyData.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, Typography } from "@mui/material";
 
 export default function EmptyData() {
@@ -6,19 +5,35 @@ export default function EmptyData() {
     <Box
       sx={{
         display: "flex",
-        gap: ".75em",
-        justifyContent: "center",
-        height: "50vh",
-        alignItems: "center",
+        flexDirection: "column",
+        gap: "1rem",
       }}
     >
-      <img
-        src={"/Homepage/emptyData.png"}
-        alt="empty-data"
-        height={60}
-        width={60}
-      />
-      <Typography sx={{ fontSize: "2rem" }}>No Data Found</Typography>
+      <Typography>Dear Visitor,</Typography>
+      <Typography>
+        The data you seek might be classified within the realm of historical
+        records, or the webpage may presently be undergoing synchronization.
+      </Typography>
+      <Typography>
+        We extend a cordial invitation to explore the capabilities of our
+        AI-driven chatbot on WhatsApp. A simple greeting of HI to{" "}
+        <a href="tel:+919374624365">+91 93746 24 365</a> initiates this seamless
+        experience.
+      </Typography>
+      <Typography>
+        For further elucidation, please don't hesitate to reach out to us via
+        phone at
+        <a href="tel:+919374624365">+91 93746 24 365</a> or through email at{" "}
+        <a href="mail:info@PolymerBazaar.com">info@PolymerBazaar.com</a>. We
+        extend a cordial invitation to explore the capabilities of our AI-driven
+        chatbot on WhatsApp. A simple greeting of HI to{" "}
+        <a href="tel:+919374624365">+91 93746 24 365</a> initiates this seamless
+        experience.
+      </Typography>
+      <Typography>
+        We express our gratitude for your perusal of PolymerBazaar. Sincerely at
+        your services 24 Hours 365 Days.
+      </Typography>
     </Box>
   );
 }
