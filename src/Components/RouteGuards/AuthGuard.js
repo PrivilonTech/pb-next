3a2ba810e5179aa36048ebc04a04644c34610596
sukrit101 <sukritsaha27@gmail.com>
2024-02-08 15:27:51 +0530

    disabling subscription check

--- src/Components/RouteGuards/AuthGuard.js
+++ src/Components/RouteGuards/AuthGuard.js
@@ -51,13 +51,14 @@ export default function AuthGuard({ children }) {
         setLoading(true);
 
         router.push("/register");
-      } else if (userLoggedIn && !isAdminCheck(userLoggedIn)) {
-        if (!isTrial(userLoggedIn) && !userLoggedIn.subscribed) {
-          setLoading(true);
+      } 
+      // else if (userLoggedIn && !isAdminCheck(userLoggedIn)) {
+      //   if (!isTrial(userLoggedIn) && !userLoggedIn.subscribed) {
+      //     setLoading(true);
 
-          router.push("/subscription");
-        }
-      }
+      //     router.push("/subscription");
+      //   }
+      // }
     }
     if (path === "/register" || path === "/subscription") {
       setLoading(false);
