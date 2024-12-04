635c5abe4c79307c5d6c5b58a72f6d9dd39a2f70
Mihir Rami <mihirrami32@gmail.com>
2024-12-04 11:47:39 +0530

    Revert "Commented markdown in blogs and changed image"
    This reverts commit e83e665392b230eb2e27cab746e17db91d4ba305.

--- src/Components/PaneContent/Blog.js
+++ src/Components/PaneContent/Blog.js
@@ -7,7 +7,7 @@ import { toast } from "react-hot-toast";
 import secureLocalStorage from "react-secure-storage";
 import Link from "next/link";
 import Image from "next/image";
-import he from "he";
+
 import Button from "../Button/Button";
 
 export default function Blog({ data, setDataChange }) {
@@ -32,8 +32,6 @@ export default function Blog({ data, setDataChange }) {
       console.log(error);
     }
   };
-
-  console.log(data.blogContent);
 
   return (
     <Box
@@ -106,9 +104,9 @@ export default function Blog({ data, setDataChange }) {
           )}
         </Box>
       </Box>
-      {/* <Box sx={{ padding: "1em 2em" }}>
+      <Box sx={{ padding: "1em 2em" }}>
         <MarkdownView markdown={data.blogContent} />
-      </Box> */}
+      </Box>
       {isImageFile && (
         <Box
           sx={{
